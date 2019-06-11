0b4cb4efed97025df261c8e3263cffa9b42519a4
Vitor Amaral <vitor.amaral@creditoo.com.br>
2019-06-11 16:05:01 -0300

    Add getters

--- src/main/groovy/br/com/creditoo/aws/responses/GenerateDataKeyResponse.groovy
+++ src/main/groovy/br/com/creditoo/aws/responses/GenerateDataKeyResponse.groovy
@@ -2,10 +2,19 @@ package br.com.creditoo.aws.responses
 
 class GenerateDataKeyResponse {
     private String ciphertext
+
     private String plainText
 
     GenerateDataKeyResponse(String ciphertext, String plainText) {
         this.ciphertext = ciphertext
         this.plainText = plainText
+    }
+
+    String getPlainText() {
+        return plainText
+    }
+
+    String getCiphertext() {
+        return ciphertext
     }
 }
